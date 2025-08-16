39460c5f81b687fe280fe4a0cd9009929c4cba96
PEVE224 <salchepevebilivogui@gmail.com>
2025-08-16 18:01:56 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,18 @@
+function getFormData(){
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  return `👋 Bonjour, je suis ${name} (${email}).\n${message}`;
+}
 
+function sendWhatsApp(){
+  let phone = "224611016900"; // ton numéro
+  let text = getFormData();
+  window.open(`https://wa.me/${phone}?text=${encodeURIComponent(text)}`, "_blank");
+}
+
+function sendSMS(){
+  let phone = "224611016900"; // ton numéro
+  let text = getFormData();
+  window.open(`sms:${phone}?body=${encodeURIComponent(text)}`, "_blank");
+}
